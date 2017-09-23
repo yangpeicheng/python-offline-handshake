923bcc7ddbc5e80af59edc4328930b43f44e0f70
yangpeicheng <18362903155@163.com>
2017-09-23 17:21:25 +0800

    second

--- quantization.py
+++ quantization.py
@@ -7,7 +7,7 @@ def getPara(data):
     var=math.sqrt(np.var(data))
     return mean,var
 
-def quantizator(value,q_plus,q_minus):
+def quantizer(value,q_plus,q_minus):
     if value>q_plus:
         return 1
     elif value<q_minus:
@@ -25,18 +25,60 @@ def handler(data_list,m,alpha):
         q_plus=mean+alpha*var
         q_minus=mean-alpha*var
         for j in range(m):
-            bit=quantizator(data_list[i+j],q_plus,q_minus)
+            bit=quantizer(data_list[i+j],q_plus,q_minus)
             bits.append(bit)
             if bit!=2:
                 index.append(i+j)
     return bits,index
 
+def levelcrossing(data_list,m,alpha):
+    bits=[]
+    index=[]
+    mean,var=getPara(data_list)
+    q_plus = mean + alpha * var
+    q_minus = mean - alpha * var
+    i=1
+    counter=1
+    preBit=quantizer(data_list[0],q_plus,q_minus)
+    while i<len(data_list):
+        bit=quantizer(data_list[i],q_plus,q_minus)
+        bits.append(bit)
+        if bit==preBit:
+            counter+=1
+        else:
+            if counter>=m:
+                start=i-m
+                end=i-1
+                index.append(int((start+end)/2))
+            counter=1
+            preBit=bit
+        i+=1
+    return bits,index
+
+def check_levelcrossing(bits,index,m):
+    checked_index=[]
+    for i in index:
+        start=i-math.floor((m-2)/2)
+        end=i+math.ceil((m-2)/2)
+        bit=bits[start]
+        flag=True
+        if end>=len(bits):
+            break
+        for j in range(start,end+1):
+            if bit!=bits[j]:
+                flag=False
+                break
+        if flag:
+            checked_index.append(i)
+    return checked_index
+
 if __name__=="__main__":
+    m=5
+    alpha=0.2
     mdata=np.mat(readData("E:\\SensorData\\pcatest\\masterlocal-9tttransform.csv"))
-    mbits,mindex=handler(mdata[:,0],5,0.2)
     sdata=np.mat(readData("E:\\SensorData\\pcatest\\slavelocal-9tttransform.csv"))
-    sbits,sindex=handler(sdata[:,0],5,0.2)
-    print(len(mindex),len(sindex),len(set(mindex).intersection(set(sindex))))
+    mbits, mindex = handler(mdata[:, 0], m, alpha)
+    sbits,sindex=handler(sdata[:,0],m,alpha)
     common=list(set(mindex).intersection(set(sindex)))
     com_mbits=[mbits[i] for i in common]
     com_sbits=[sbits[i] for i in common]
@@ -45,3 +87,12 @@ if __name__=="__main__":
         if com_mbits[i]!=com_sbits[i]:
             counter+=1
     print(counter)
+
+    m_levelcrossing_bits,m_levelcrossing_index=levelcrossing(mdata[:,1],m, alpha)
+    s_levelcrossing_bits,s_levelcrossing_index = levelcrossing(sdata[:, 1], m, alpha)
+    checked_index=check_levelcrossing(s_levelcrossing_bits,m_levelcrossing_index,m)
+    print(m_levelcrossing_index)
+    print(s_levelcrossing_index)
+    print(checked_index)
+    print([m_levelcrossing_bits[i] for i in checked_index])
+    print([s_levelcrossing_bits[i] for i in checked_index])
--- subData.py
+++ subData.py
@@ -1,9 +1,7 @@
 import csv
-
-
-def extract(m):
-    start=85
-    end=229
+import math
+import numpy as np
+def extract(m,start,end):
     new = m.split('.')[0] + "t.csv"
     output=open(new,'w',newline="")
     write=csv.writer(output)
@@ -15,7 +13,71 @@ def extract(m):
             if index>start and index<end:
                 write.writerow(line)
 
-extract("E:\\SensorData\\pcatest\\masterlocal-9.csv")
-extract("E:\\SensorData\\pcatest\\slavelocal-9.csv")
+def init(data):
+    window=[]
+    window_size=5
+    magnitude=[]
+    variance=[]
+    for i in data:
+        mag=getMagnitude(i)
+        window.append(mag)
+        if len(window)==window_size:
+            magnitude.append(mag)
+            variance.append(np.var(window))
+            window.remove(0)
+    return magnitude,variance
+
+def getMagnitude(data):
+    sum=0
+    for i in data:
+        sum+=i*i
+    return math.sqrt(sum)
+
+#data ---acceleration data
+def findHandShake(data):
+    magnitude,variance=init(data)
+    MAGNITUDE_THRESHOLD = 1.0
+    VARIANCE_THRESHOLD = 0.2
+    preState=True
+    downIndex=[]
+    upIndex=[]
+    for i in range(len(magnitude)):
+        currentState=(variance[i]<VARIANCE_THRESHOLD) and (magnitude[i]<MAGNITUDE_THRESHOLD)
+        if preState==True and currentState==False:
+            downIndex.append(i)
+        elif preState==False and currentState==True:
+            upIndex.append(i)
+        preState=currentState
+    i=0
+    while i<min(len(upIndex),len(downIndex)-1):
+        if downIndex[i+1]-upIndex[i]<5:
+            downIndex.remove(i+1)
+            upIndex.remove(i)
+        else:
+            i+=1
+    start=downIndex[0]
+    end=upIndex[0]
+    length=end-start
+    for j in range(1,min(len(upIndex),len(downIndex))):
+        if upIndex[j]-downIndex[j]>length:
+            length=upIndex[j]-downIndex[j]
+            end=upIndex[j]
+            start=downIndex[j]
+    return start,end
+
+def readCompleteData(filename):
+    acceleration=[]
+    with open(filename) as f:
+        reader=csv.reader(f)
+        for line in reader:
+            acceleration.append([float(line[x]) for x in range(3,6)])
+    return acceleration
+
+
+
+
+if __name__=="__main__":
+    extract("E:\\SensorData\\pcatest\\masterlocal-9.csv")
+    extract("E:\\SensorData\\pcatest\\slavelocal-9.csv")
 
 
